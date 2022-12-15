bd89fe4714aa18d68d1329958fb606a94ca55e46
joycexzhao <120589697+joycexzhao@users.noreply.github.com>
2022-12-15 01:24:48 -0600

    Add files via upload

---
+++ 2048_final_project/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/zhao/Desktop/211 C++/2048_final_project
+# Build directory: /Users/zhao/Desktop/211 C++/2048_final_project/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test_model_test "model_test")
+set_tests_properties(Test_model_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/zhao/Desktop/211 C++/2048_final_project/.cs211/cmake/211commands.cmake;90;add_test;/Users/zhao/Desktop/211 C++/2048_final_project/CMakeLists.txt;21;add_test_program;/Users/zhao/Desktop/211 C++/2048_final_project/CMakeLists.txt;0;")
+subdirs(".cs211/lib/catch")
+subdirs(".cs211/lib/ge211")
